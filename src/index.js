34515e8886d41784fce4e16439876dc8b449de13
Vaishali V <sweetchoco483@gmail.com>
2023-08-05 10:17:55 +0530

    new

--- src/index.js
+++ src/index.js
@@ -4,10 +4,33 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+class StateManagement extends React.Component{
+  constructor(){
+    super()
+    this.state = ({os: "Windows"})
+  }
+  windows = () => this.setState = {os: "Windows"}
+  mac = () => this.setState = {os: "Mac"}
+  linux = () => this.setState = os.{os: "Linux"}
+
+  render(){
+    return(
+      <div>
+        <h1>I am using {this.state.os} Operating System</h1>
+        <button onClick={this.mac}>Change to Mac</button>
+        <button onClick={this.linux}>Change to Linux</button>
+        <button onClick={this.windows}>Change to Windows</button>
+        <br></br><br></br>
+        <App></App>
+      </div>
+    )
+  }
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StateManagement />
   </React.StrictMode>
 );
 
